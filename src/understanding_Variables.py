b7c59a69788d43b198142d5d5f1ef3b47666fb81
Enrique rivera <2530389@upv.edu.mx>
2025-11-17 11:36:54 -0600

    aprendimos las listas en python

---
+++ src/understanding_Variables.py
@@ -0,0 +1,24 @@
+message_enrique = " Hola amigo python "
+print (message_enrique)
+
+
+"""
+Traceback: Es un registro de donde el ineterprete tuvo problemas al intentar ejecutar su codigo
+"""
+
+"""
+Name error: Sigmifica que olvidamos establecer el valor de una variable antes de 
+utilizar o metemos el error al inegresar el  nombre de una variable
+"""
+
+"""
+Ejercicio:
+1.Almacena un mensaje e imrpimelo
+2.alamecena un menssaje en una variable e imprimelo.luego cambia el valor de tu variable a
+    otro mensaje e imrpime el nuevo mensaje
+"""
+
+message = "Hello world"
+print(message)
+message = "Hello universe"
+print(message)  
