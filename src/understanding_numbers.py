1547757e0580aeb3742e5bc2dd801caa8bf43364
Enrique rivera <2530389@upv.edu.mx>
2025-11-17 12:33:46 -0600

    Aprendimos sobre los numeron en python

---
+++ src/understanding_numbers.py
@@ -0,0 +1,51 @@
+#number
+
+"""
+Enteros 
+
+los enteros los podemos sumar, restar, multiplicar y dividir
+tambienpudemos poner potencias con (**) y modulo con (%)
+"""
+number1 = 35
+number2 = 15
+suma=number1 + number2
+resta=number1 - number2
+multiplicacion=number1 * number2
+divicion=number1 / number2
+potencia=number1 **2
+modulo=number1 % number2
+print("suma:", suma, type(suma))
+print("resta:",resta, type(resta))
+print("multiplicacion:",multiplicacion, type(multiplicacion))
+print("divicion:",divicion,type(divicion))  
+print("putencia:",potencia,type(potencia))  
+print("modulo:",modulo,type(modulo))
+
+
+"""
+ jerarquias de operaciones
+
+"""
+
+"""
+flotantes
+python llama flotantes a los numeros con decimales
+ 
+ los podemos sumar, restar, multiplicar y dividir
+tambienpudemos poner potencias con (**)
+
+"""
+
+print(0.1 + 0.1)
+print(0.2 - 0.2)
+print(0.1 * 2)
+print(2*0.2)
+
+#imprimir edad
+edad = 25
+message = "charly tiene " + str(edad) + " años"
+print(message)
+"""
+type error
+esto significa que python no puede reconocer el tipo de informacion que le estamos dando
+"""
